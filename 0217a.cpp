15e93b5599f5d9201fe45992240b10a62045f070
Lightsource <142080508+l1ghtsource@users.noreply.github.com>
2023-09-17 20:31:09 +0300

    Add files via upload

---
+++ 0217a.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <set>
+
+bool check(int num) {
+	int l = 0;
+	std::set<int> digits;
+	while (num > 0) {
+		digits.insert(num % 10);
+		num /= 10;
+		l += 1;
+	}
+	if (digits.size() == l) {
+		return false;
+	}
+	else {
+		return true;
+	}
+}
+
+int main() {
+	int y;
+	std::cin >> y;
+	y++;
+	while (check(y)) {
+		y++;
+	}
+	std::cout << y;
+	return 0;
+}
