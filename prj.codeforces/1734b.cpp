348a3d3e159996143cbf389c95209d517625d917
Lightsource <142080508+l1ghtsource@users.noreply.github.com>
2023-10-09 22:56:10 +0300

    Add files via upload

---
+++ prj.codeforces/1734b.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+void solve() {
+  int n = 0;
+  std::cin >> n;
+  if (n == 1) {
+    std::cout << 1 << std::endl;
+  }
+  else if (n == 2) {
+    std::cout << 1 << std::endl;
+    std::cout << 1 << ' ' << 1 << std::endl;
+  }
+  else {
+    std::cout << 1 << std::endl;
+    std::cout << 1 << ' ' << 1 << std::endl;
+    for (int i = 1; i < n - 1; ++i) {
+      std::cout << 1 << ' ';
+      for (int j = 0; j < i; ++j) {
+        std::cout << 0 << ' ';
+      }
+      std::cout << 1 << ' ' << std::endl;
+    }
+  }
+}
+
+int main() {
+  int t = 0;
+  std::cin >> t;
+  while (t--) {
+    solve();
+  }
+  return 0;
+}
