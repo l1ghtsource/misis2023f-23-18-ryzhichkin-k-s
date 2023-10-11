6ae2db9858d94241a1ec003ff0b98dfd3341ee7e
Lightsource <142080508+l1ghtsource@users.noreply.github.com>
2023-10-11 23:26:27 +0300

    Update 0276a.cpp

--- prj.codeforces/0276a.cpp
+++ prj.codeforces/0276a.cpp
@@ -1,21 +1,27 @@
 #include <iostream>
 
 int main() {
-	int n, k, f, t, p, m;
-	m = -100000000000000000;
-	std::cin >> n >> k;
-	for (int i = 0; i < n; i++) {
-		std::cin >> f >> t;
-		if (t > k) {
-			p = f - (t - k);
-		}
-		else {
-			p = f;
-		}
-		if (p > m) {
-			m = p;
-		}
-	}
-	std::cout << m;
-	return 0;
+  int n, k, f, t, p, m;
+  int n = 0; // кол-во ресторанов
+  int k = 0; // кол-во единиц времени
+  int f = 0; // безымянная характеристика
+  int t = 0; // время, которое кролики затратят на обед в i-ом ресторане
+  int p = 0; // удовольствие
+  int m = 0; // максимальное кроличье удовольствие
+  m = -100000000000000000;
+  std::cin >> n >> k;
+  for (int i = 0; i < n; i++) {
+    std::cin >> f >> t;
+    if (t > k) {
+      p = f - (t - k);
+    }
+    else {
+      p = f;
+    }
+    if (p > m) {
+      m = p;
+    }
+  }
+  std::cout << m;
+  return 0;
 }
