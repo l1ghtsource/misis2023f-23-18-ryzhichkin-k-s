854e3bb8e1cea4b98ced6aea1c9affe3ba21d88f
Lightsource <142080508+l1ghtsource@users.noreply.github.com>
2023-10-12 00:03:34 +0300

    Update 1559a.cpp

--- prj.codeforces/1559a.cpp
+++ prj.codeforces/1559a.cpp
@@ -3,9 +3,9 @@
 #include <algorithm>
 
 void solve() {
-  int n = 0;
+  int n = 0; // длина массива
   std::cin >> n;
-  std::vector<int> arr(n);
+  std::vector<int> arr(n); // массив
   for (int i = 0; i < n; ++i) {
     std::cin >> arr[i];
   }
@@ -14,12 +14,12 @@ void solve() {
       arr[i] = arr[i] & arr[j];
     }
   }
-  int ans = *std::min_element(begin(arr), end(arr));
+  int ans = *std::min_element(begin(arr), end(arr)); // минимально возможное значение максимума
   std::cout << ans << std::endl;
 }
 
 int main() {
-  int t = 0;
+  int t = 0; // количество наборов входных данных
   std::cin >> t;
   while (t--) {
     solve();
