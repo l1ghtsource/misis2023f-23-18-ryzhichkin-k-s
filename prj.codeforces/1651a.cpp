b056ec73a5a2ad54885ddd49f665117f84bccc36
Lightsource <142080508+l1ghtsource@users.noreply.github.com>
2023-10-12 00:11:04 +0300

    Update 1651a.cpp

--- prj.codeforces/1651a.cpp
+++ prj.codeforces/1651a.cpp
@@ -2,11 +2,12 @@
 #include <cmath>
 
 int main() {
-	int t, n;
-	std::cin >> t;
-	for (int i = 0; i < t; i++) {
-		std::cin >> n;
-		std::cout << (int)pow(2, n) - 1 << std::endl;
-	}
-	return 0;
+  int t = 0; // количество наборов входных данных
+  int n = 0; // задает кол-во спортсменов = 2^n
+  std::cin >> t;
+  for (int i = 0; i < t; i++) {
+    std::cin >> n;
+    std::cout << (int)pow(2, n) - 1 << std::endl;
+  }
+  return 0;
 }
