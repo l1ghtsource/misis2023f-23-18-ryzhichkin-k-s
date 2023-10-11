689ef9af943e49ccf683c7fae869a9c10728be1a
Lightsource <142080508+l1ghtsource@users.noreply.github.com>
2023-10-11 23:28:08 +0300

    Update 0617a.cpp

--- prj.codeforces/0617a.cpp
+++ prj.codeforces/0617a.cpp
@@ -1,13 +1,14 @@
 ﻿﻿#include <iostream>
 
 int main() {
-	int x;
-	std::cin >> x;
-	if (x % 5 == 0) {
-		std::cout << x / 5;
-	}
-	else {
-		std::cout << x / 5 + 1;
-	}
-	return 0;
+  int x = 0; // координата дома
+  std::cin >> x;
+  // ищем расстояние до 0
+  if (x % 5 == 0) {
+    std::cout << x / 5;
+  }
+  else {
+    std::cout << x / 5 + 1;
+  }
+  return 0;
 }
