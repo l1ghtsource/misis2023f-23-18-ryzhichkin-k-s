5cbff61f20246c219a5a86609103c3e485eed5ee
Lightsource <142080508+l1ghtsource@users.noreply.github.com>
2023-10-11 23:57:11 +0300

    Update 1438a.cpp

--- prj.codeforces/1438a.cpp
+++ prj.codeforces/1438a.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
-#include <vector>
 
 int main() {
-  int t = 0;
+  int t = 0; // количество наборов входных данных
   std::cin >> t;
   while (t--) {
-    int n = 0;
+    int n = 0; // длина массива
     std::cin >> n;
+    // массив {1, 1, ..., 1} (n единиц), очевидно, является прекрасным
     for (int i = 0; i < n; ++i) {
       std::cout << 1 << ' ';
     }
