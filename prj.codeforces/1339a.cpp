a6bd8a682e4aee2d9d835c30617d8535b0d1c62c
Lightsource <142080508+l1ghtsource@users.noreply.github.com>
2023-10-12 23:00:05 +0300

    Add files via upload

---
+++ prj.codeforces/1339a.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+void solve() {
+	int n = 0;
+	std::cin >> n;
+	std::cout << n << std::endl;
+}
+
+int main() {
+	int t = 0;
+	std::cin >> t;
+	while (t--) {
+		solve();
+	}
+}
