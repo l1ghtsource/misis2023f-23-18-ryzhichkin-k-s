cbaeba70e38ee6c5d429691aaa407b5c8f21894f
Lightsource <142080508+l1ghtsource@users.noreply.github.com>
2023-10-13 22:14:34 +0300

    Update 1670b.cpp

--- prj.codeforces/1670b.cpp
+++ prj.codeforces/1670b.cpp
@@ -1,32 +1,31 @@
-#include <iostream>
 #include <vector>
 
 int main() {
-    std::ios::sync_with_stdio(false); // ñïàñëî, áåç ýòîãî TL íà 35-ì òåñòå
-    int t = 0;
-    std::cin >> t;
-    for (int i = 0; i < t; i++) {
-        int n = 0;
-        std::cin >> n;
-        std::string s;
-        std::cin >> s;
-        int k = 0;
-        std::cin >> k;
-        std::vector<bool> spec(30, false);
-        int res = 0;
-        int dr = 0;
-        for (int j = 0; j < k; j++) {
-            char c = ' ';
-            std::cin >> c;
-            spec[c - 'a'] = true;
-        }
-        for (int r = 0; r < n; r++) {
-            if (spec[s[r] - 'a']) {
-                res = std::max(res, r - dr);
-                dr = r;
-            }
-        }
-        std::cout << res << std::endl;
+  std::ios::sync_with_stdio(false);
+  int t = 0; // количество наборов входных данных
+  std::cin >> t; 
+  for (int i = 0; i < t; i++) {
+    int n = 0; // длина пароля
+    std::cin >> n;
+    std::string s; // пароль
+    std::cin >> s;
+    int k = 0; // количество особых букв
+    std::cin >> k;
+    std::vector<bool> spec(30, false); // особые буквы
+    int res = 0;
+    int dr = 0;
+    for (int j = 0; j < k; j++) {
+      char c = ' ';
+      std::cin >> c; // очередная особа буква
+      spec[c - 'a'] = true;
     }
-    return 0;
+    for (int r = 0; r < n; r++) {
+      if (spec[s[r] - 'a']) {
+        res = std::max(res, r - dr);
+        dr = r;
+      }
+    }
+    std::cout << res << std::endl;
+  }
+  return 0;
 }
