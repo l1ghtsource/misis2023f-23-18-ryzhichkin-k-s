ce9bc008e345eb3b13bb8130181bfb4685ba0b33
Lightsource <142080508+l1ghtsource@users.noreply.github.com>
2023-10-13 22:09:25 +0300

    Update 1553a.cpp

--- prj.codeforces/1553a.cpp
+++ prj.codeforces/1553a.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 
 void solve() {
-	int n = 0;
-	std::cin >> n;
-	std::cout << ((n + 1) / 10) << std::endl;
+  int n = 0; // верхняя граница
+  std::cin >> n;
+  std::cout << ((n + 1) / 10) << std::endl; // нам подходят числа вида a0a1a2...an, где an = 9, таких чисел на отрезке [1, n] ровно (n + 1) // 10.
 }
 
 int main() {
-	int t = 0;
-	std::cin >> t;
-	while (t--) {
-		solve();
-	}
+  int t = 0; // количество наборов входных данных
+  std::cin >> t;
+  while (t--) {
+    solve();
+  }
 }
