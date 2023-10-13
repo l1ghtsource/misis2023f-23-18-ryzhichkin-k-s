12d51c4466b83d1e76e8854beed11a6e7ea68615
Lightsource <142080508+l1ghtsource@users.noreply.github.com>
2023-10-13 22:37:39 +0300

    Update 1872a.cpp

--- prj.codeforces/1872a.cpp
+++ prj.codeforces/1872a.cpp
@@ -2,15 +2,15 @@
 #include <cmath>
 
 int main() {
-	int t, a, b, c, ans;
-	double delta = 0, n = 0;
-	std::cin >> t;
-	for (int i = 0; i < t; i++) {
-		std::cin >> a >> b >> c;
-		delta = abs(a - b);
-		n = delta / (2 * c);
-		std::cout << ceil(n) << std::endl;
+  int t, a, b, c, ans;
+  double delta = 0, n = 0;
+  std::cin >> t;
+  for (int i = 0; i < t; i++) {
+    std::cin >> a >> b >> c;
+    delta = abs(a - b);
+    n = delta / (2 * c);
+    std::cout << ceil(n) << std::endl;
 
-	}
-	return 0;
+  }
+  return 0;
 }
