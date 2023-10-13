68029f1d3704197939e0bb03adaa8beb17060e4c
Lightsource <142080508+l1ghtsource@users.noreply.github.com>
2023-10-13 22:32:40 +0300

    Update 1849a.cpp

--- prj.codeforces/1849a.cpp
+++ prj.codeforces/1849a.cpp
@@ -2,40 +2,40 @@
 #include <vector>
 
 int main() {
-	int t, b, c, h, curr_bread, curr_others, counter;
-	std::vector<int> breads;
-	std::vector<int> others;
-	std::cin >> t;
-	counter = 0;
-	for (int i = 0; i < t; i++) {
-		std::cin >> b >> c >> h;
-		breads.push_back(b);
-		others.push_back(c + h);
-	}
-	for (int j = 0; j < t; j++) {
-		curr_bread = breads[j];
-		curr_others = others[j];
-		curr_bread -= 2;
-		curr_others -= 1;
-		counter += 3;
-		while (true) {
-			if (curr_bread > 0 && curr_others > 0) {
-				curr_bread--;
-				counter++;
-			}
-			else {
-				break;
-			}
-			if (curr_others > 0) {
-				curr_others--;
-				counter++;
-			}
-			else {
-				break;
-			}
-		}
-		std::cout << counter << std::endl;
-		counter = 0;
-	}
-	return 0;
+  int t, b, c, h, curr_bread, curr_others, counter;
+  std::vector<int> breads;
+  std::vector<int> others;
+  std::cin >> t;
+  counter = 0;
+  for (int i = 0; i < t; i++) {
+    std::cin >> b >> c >> h;
+    breads.push_back(b);
+    others.push_back(c + h);
+  }
+  for (int j = 0; j < t; j++) {
+    curr_bread = breads[j];
+    curr_others = others[j];
+    curr_bread -= 2;
+    curr_others -= 1;
+    counter += 3;
+    while (true) {
+      if (curr_bread > 0 && curr_others > 0) {
+        curr_bread--;
+        counter++;
+      }
+      else {
+        break;
+      }
+      if (curr_others > 0) {
+        curr_others--;
+        counter++;
+      }
+      else {
+        break;
+      }
+    }
+    std::cout << counter << std::endl;
+    counter = 0;
+  }
+  return 0;
 }
