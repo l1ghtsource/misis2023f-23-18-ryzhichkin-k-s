6c8d5ed8059cb0f3a32912f1a12f0bcb3e62edc0
Lightsource <142080508+l1ghtsource@users.noreply.github.com>
2023-10-20 09:56:03 +0300

    Update 1512b.cpp

--- prj.codeforces/1512b.cpp
+++ prj.codeforces/1512b.cpp
@@ -84,16 +84,10 @@ void solve() {
       y_ans_2 = y_2 - d;
     }
     else if (x_1 > x_2 && y_2 < y_1) {
-      x_ans_1 = x_1;
-      y_ans_1 = y_1 + d;
-      x_ans_2 = x_2;
-      y_ans_2 = y_2 + d;
+      // не успел :(
     }
     else if (x_1 < x_2 && y_2 < y_1) {
-      x_ans_1 = x_1;
-      y_ans_1 = y_1 + d;
-      x_ans_2 = x_2;
-      y_ans_2 = y_2 + d;
+      // не успел :(
     }
   }
   std::vector<std::vector<char>> a(n, std::vector<char>(n, '.'));
