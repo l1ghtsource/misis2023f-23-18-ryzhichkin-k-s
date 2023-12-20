7be94722b6d66f1e0b9e9f6af6c577396812ed9e
lightsource <142080508+l1ghtsource@users.noreply.github.com>
2023-12-20 22:03:19 +0300

    Add files via upload

--- prj.lab/dynarr/dynarr.hpp
+++ prj.lab/dynarr/dynarr.hpp
@@ -5,62 +5,33 @@
 
 #include <iostream>
 #include <cstddef>
+#include <stdexcept>
 
 class DynArr {
 public:
   [[nodiscard]] DynArr() = default;
-  [[nodiscard]] DynArr(const DynArr&) = default;
-  [[nodiscard]] DynArr(const ptrdiff_t size) : size_(size), capacity_(size) {
-    if (size <= 0) {
-      throw std::invalid_argument("Zero size is not allowed in DynArr ctor");
+  [[nodiscard]] DynArr(const DynArr&);
+  [[nodiscard]] DynArr(const ptrdiff_t size);
+
+  ~DynArr();
+
+  DynArr& operator=(const DynArr& rhs) {
+    if (&rhs != this) {
+      Resize(rhs.Size());
+      for (int i = 0; i < rhs.Size(); ++i) {
+        *(data_ + i) = rhs[i];
+      }
     }
-    else {
-      data_ = new float[size] {0};
-    }
+    return *this;
   }
-
-  ~DynArr() = default;
-
-  [[nodiscard]] DynArr& operator=(const DynArr&) = default;
 
   [[nodiscard]] ptrdiff_t Size() const noexcept { return size_; }
 
-  void Resize(const ptrdiff_t size) {
-    if (size > capacity_) {
-      int new_capacity = size * 2;
-      float* new_data = new float[new_capacity] {0};
-      for (int i = 0; i < size_; ++i)
-        new_data[i] = data_[i];
-      delete[] data_;
-      data_ = new_data;
-      capacity_ = new_capacity;
-    }
-    size_ = size;
-  }
+  void Resize(const ptrdiff_t size);
 
-  [[nodiscard]] float& operator[](const ptrdiff_t idx) { 
-    if (idx < 0) {
-      throw std::invalid_argument("Negative index is not allowed in [] operator");
-    }
-    else if (idx >= size_) {
-      throw std::invalid_argument("Index that greater than size is not allowed in [] operator");
-    }
-    else {
-      return data_[idx];
-    }
-  }
+  [[nodiscard]] float& operator[](const ptrdiff_t idx);
+  [[nodiscard]] const float& operator[](const ptrdiff_t idx) const;
 
-  [[nodiscard]] const float& operator[](const ptrdiff_t idx) const {
-    if (idx < 0) {
-      throw std::invalid_argument("Negative index is not allowed in [] operator");
-    }
-    else if (idx >= size_) {
-      throw std::invalid_argument("Index that greater than size is not allowed in [] operator");
-    }
-    else {
-      return data_[idx];
-    }
-  }
 private:
   ptrdiff_t size_ = 0;
   ptrdiff_t capacity_ = 0;
